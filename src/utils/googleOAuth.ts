282d375f2727e6eb7281181c174a3117c61ca305
Promise Ibeh <118574387+ibeh-promise@users.noreply.github.com>
2025-12-12 20:48:06 +0000

    removed the appwrite definition

--- src/utils/googleOAuth.ts
+++ src/utils/googleOAuth.ts
@@ -1,12 +1,12 @@
-import { Client, Account} from 'appwrite';
+// import { Client, Account} from 'appwrite';
 
-const client = new Client();
+// const client = new Client();
 
-client
-    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
-    .setProject('693b6e610017df14e3fb'); // Replace with your project ID
+// client
+//     .setEndpoint('https://nyc.cloud.appwrite.io/v1')
+//     .setProject('693b6e610017df14e3fb'); // Replace with your project ID
 
-const account = new Account(client);
+// const account = new Account(client);
 
-export { account, client};
-export { ID } from 'appwrite';
+// export { account, client};
+// export { ID } from 'appwrite';
