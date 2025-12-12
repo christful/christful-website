4f54a52a63e7c4015e333567f8e19c540ae13ee3
Promise Ibeh <118574387+ibeh-promise@users.noreply.github.com>
2025-12-12 00:59:30 +0000

    fixed the peremeter issue

---
+++ src/api/authApi.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = "https://christful-backend.vercel.app"; // replace with your backend URL
+
+export interface RegisterData {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export const registerUser = async (userData: RegisterData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+export const loginUser = async (credentials: LoginData) => {
+  const response = await axios.post(`${API_URL}/login`, credentials);
+  return response.data;
+};
